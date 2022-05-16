90417006fd9bd637e32de4a97b4cc87e2192f7d0
ahad abdul <ahad.a2703@gmail.com>
2022-05-16 21:04:21 +1000

    canvas size UI

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const gridContainer = document.querySelector(".grid-container");
 const eraserBtn = document.querySelector(".eraser-btn");
 const drawBtn = document.querySelector(".draw-btn");
 
+const smallPixelBtn = document.querySelector(".canvas-small-btn");
+const mediumPixelBtn = document.querySelector(".canvas-medium-btn");
+const largePixelBtn = document.querySelector(".canvas-large-btn");
+
+let canvasSize = "canvas-small";
 let bToggleActiveDraw = false;
 let bToggleEraser = false;
 //let i = 1;
-for (let j = 0; j < 3000; j += 1) {
+for (let j = 0; j < 12000; j += 1) {
   gridContainer.insertAdjacentHTML("beforeend", `<div class="grid-item"></div>`);
   const gridItem = gridContainer.lastElementChild;
   //i++;
@@ -20,6 +25,18 @@ function activeDrawingTool(gridElement) {
   }
   else if (bToggleEraser) {
     gridElement.style.backgroundColor = "white";
+  }
+}
+
+function createCanvasBoard(rowSize ) {
+  gridContainer.innerHTML = '';
+  for (let j = 0; j < rowSize; j += 1) {
+    gridContainer.insertAdjacentHTML("beforeend", `<div class="grid-item"></div>`);
+    const gridItem = gridContainer.lastElementChild;
+    //i++;
+      gridItem.addEventListener('mouseenter', (event)=> {
+        activeDrawingTool(gridItem);
+      })
   }
 }
 
@@ -41,13 +58,40 @@ document.addEventListener("keydown", (e) => {
   else if (e.code == "KeyE") {
     toggleEraser();
   }
-})
-
+});
 
 drawBtn.addEventListener("click", (e)=> {
   toggleActiveDraw();
-})
+});
 
 eraserBtn.addEventListener("click", (e)=> {
   toggleEraser();
-})
+});
+
+smallPixelBtn.addEventListener("click", (e) => {
+  if (!(gridContainer.classList.contains("canvas-small"))) {
+    gridContainer.classList.toggle(canvasSize);
+    gridContainer.classList.toggle("canvas-small");
+    canvasSize = "canvas-small";
+    createCanvasBoard(12000);
+  }
+});
+
+mediumPixelBtn.addEventListener("click", (e) => {
+  if (!(gridContainer.classList.contains("canvas-medium"))) {
+    gridContainer.classList.toggle(canvasSize);
+    gridContainer.classList.toggle("canvas-medium");
+    canvasSize = "canvas-medium";
+    createCanvasBoard(3000);
+  }
+});
+
+largePixelBtn.addEventListener("click", (e) => {
+  if (!(gridContainer.classList.contains("canvas-large"))) {
+    gridContainer.classList.toggle(canvasSize);
+    gridContainer.classList.toggle("canvas-large");
+    canvasSize = "canvas-large";
+    createCanvasBoard(750);
+
+  }
+});
